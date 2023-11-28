7e671456519083fc2bbd4e9d98c3cf3c87383d49
«riart5» <ribotartur@gmail.com>
2023-11-28 13:19:04 +0100

    he cambiado el texto y el rango

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 
-for i in range(0,33):
-    print("Primero estar atento, luego copiar")
+for i in range(0,20):
+    print("Primero estar atento, luego copiar y luego entender")
     
